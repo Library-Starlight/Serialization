28464d7185bbfaee1369e91ce43466248730f264
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-05-13 16:54:10 +0800

    feature
    Linq to Json

--- src/JsonSerialization/JsonSerialization/Program.cs
+++ src/JsonSerialization/JsonSerialization/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +14,41 @@ namespace JsonSerialization
     {
         static void Main(string[] args)
         {
-            SerializeDateTimeInSpecificFormat();
+            ReadNodeValue();
 
             Console.ReadLine();
         }
+
+        #region 读取节点值
+
+        /// <summary>
+        /// 使用Linq to Json读取节点值
+        /// </summary>
+        private static async void ReadNodeValue()
+        {
+            using (var fs = new FileStream("Text/PackingSpaceResponse.json", FileMode.Open, FileAccess.Read))
+            using (var sr = new StreamReader(fs))
+            {
+                var json = await sr.ReadToEndAsync();
+
+                var jObj = JObject.Parse(json);
+
+                Console.WriteLine($"原文：");
+                Console.WriteLine(jObj.ToString(Formatting.Indented));
+
+                // 子节点
+
+                Console.WriteLine($"serviceId：{jObj["serviceId"]}");
+                Console.WriteLine($"dataItems：{jObj["dataItems"]}");
+
+                Console.WriteLine($"dataItems[0]：{jObj["dataItems"][0]}");
+                Console.WriteLine($"dataItems[0].objectId：{jObj["dataItems"][0]["operateType"]}");
+            }
+        }
+
+        #endregion
+
+        #region 将时间序列化为指定格式
 
         private static async void SerializeDateTimeInSpecificFormat()
         {
@@ -30,6 +63,10 @@ namespace JsonSerialization
                 Console.WriteLine(json);
             }
         }
+
+        #endregion
+
+        #region 序列化 / 反序列化嵌套的泛型类型
 
         /// <summary>
         /// 序列化嵌套泛型类型
@@ -75,5 +112,7 @@ namespace JsonSerialization
                 Console.WriteLine(response.dataItems.First().attributes.totalSpace);
             }
         }
+
+        #endregion
     }
 }
