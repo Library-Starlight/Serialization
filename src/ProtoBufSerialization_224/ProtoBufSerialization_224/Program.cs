e889088e433268a69c9d5ee408c4093424c90e69
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-08-29 22:03:31 +0800

    feature
    PB2.4.4版本序列化

---
+++ src/ProtoBufSerialization_224/ProtoBufSerialization_224/Program.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json;
+using ProtoBuf;
+using ProtoBuf.Meta;
+using ProtoBufSerialization_224.Serializers;
+using System;
+using System.IO;
+
+namespace ProtoBufSerialization_224
+{
+
+    public enum ModelState
+    {
+        V1 = 1,
+        V2 = 2,
+        V3 = 3,
+    }
+
+    [ProtoContract]
+    public class DemoModel
+    {
+        [ProtoMember(1)]
+        public ModelState Value { get; set; }
+        [ProtoMember(2)]
+        public string S { get; set; }
+        [ProtoMember(3)]
+        public uint V1 { get; set; }
+        [ProtoMember(4)]
+        public bool Result { get; set; }
+        [ProtoMember(5)]
+        public int V5 { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Begin");
+            Console.WriteLine();
+
+            byte[] data;
+            DemoModel model;
+            byte[] data1;
+            MemoryStream ms;
+            ProtoWriter writestate;
+            var typeModel = RuntimeTypeModel.Create();
+
+            // Bool
+            ms = new MemoryStream();
+            writestate = ProtoBuf.ProtoWriter.Create(ms, typeModel);
+            ProtoWriter.WriteFieldHeader(4, WireType.Variant, writestate);
+            ProtoWriter.WriteBoolean(true, writestate);
+            writestate.Close();
+            ms.Position = 0;
+
+            data = ms.ToArray();
+            model = new DemoModel { Result = true };
+            data1 = ProtoBufSerializer.Serialize(model);
+            ms.Close();
+            ms = null;
+
+            Print("bool");
+
+            // Int32
+            ms = new MemoryStream();
+            writestate = ProtoBuf.ProtoWriter.Create(ms, typeModel);
+            ProtoWriter.WriteFieldHeader(5, WireType.Variant, writestate);
+            ProtoWriter.WriteInt32(300, writestate);
+            writestate.Close();
+            ms.Position = 0;
+
+            data = ms.ToArray();
+            model = new DemoModel { V5 = 300 };
+            data1 = ProtoBufSerializer.Serialize(model);
+            ms.Close();
+            ms = null;
+
+            Print("int32");
+
+            // Uint32
+            ms = new MemoryStream();
+            writestate = ProtoBuf.ProtoWriter.Create(ms, typeModel);
+            ProtoWriter.WriteFieldHeader(3, WireType.Variant, writestate);
+            ProtoWriter.WriteUInt32(12345, writestate);
+            writestate.Close();
+            ms.Position = 0;
+
+            data = ms.ToArray();
+            model = new DemoModel { V1 = 12345 };
+            data1 = ProtoBufSerializer.Serialize(model);
+            ms.Close();
+            ms = null;
+
+            Print("uint32");
+
+            Console.WriteLine();
+            Console.WriteLine($"End");
+
+            // Int32
+            
+
+            void Print(string s)
+            {
+                Console.WriteLine($"{s} - wri: {BitConverter.ToString(data)}");
+                Console.WriteLine($"{s} - ser: {BitConverter.ToString(data1)}");
+            }
+        }
+
+        static void Introduce()
+        {
+            //var writer = new ProtoBuf.ProtoWriter();
+
+            var typeModel = RuntimeTypeModel.Create();
+            using var ms = new MemoryStream();
+            var state = ProtoBuf.ProtoWriter.Create(ms, typeModel);
+            //state.WriteFieldHeader(10, ProtoBuf.WireType.Fixed64);
+            //state.WriteUInt64(10000);
+
+            ProtoWriter.WriteFieldHeader(25, ProtoBuf.WireType.Variant, state);
+            ProtoWriter.WriteBoolean(true, state);
+
+            state.Close();
+
+            ms.Position = 0;
+            var data = ms.ToArray();
+            Console.WriteLine(BitConverter.ToString(data));
+
+            //MessageParser parser = 
+            //IMessage message = parser.ParseFrom(new byte[] { 0xC8, 0x01, 0x01 });
+
+            //var descriptor = message.Descriptor;
+            //foreach (var field in descriptor.Fields.InDeclarationOrder())
+            //{
+            //    Console.WriteLine(
+            //        "Field {0} ({1}): {2}",
+            //        field.FieldNumber,
+            //        field.Name,
+            //        field.Accessor.GetValue(message));
+            //}
+        }
+    }
+}
---
+++ src/ProtoBufSerialization_224/ProtoBufSerialization_224/Serializers/ProtoBufSerializer.cs
@@ -0,0 +1,168 @@
+﻿using ProtoBuf;
+using ProtoBuf.Meta;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ProtoBufSerialization_224.Serializers
+{
+    /// <summary>
+    /// ProtoBuf序列化
+    /// </summary>
+    public static class ProtoBufSerializer
+    {
+        // TODO: 用合理的方法重写重复部分
+
+        /// <summary>
+        /// 类型模型
+        /// </summary>
+        private readonly static RuntimeTypeModel _typeModel = RuntimeTypeModel.Create();
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="obj">对象实例</param>
+        /// <returns></returns>
+        public static byte[] Serialize<T>(T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                // 序列化到内存中
+                Serializer.Serialize(ms, obj);
+
+                // 读取到字节数组
+                ms.Position = 0;
+                var data = new byte[ms.Length];
+                ms.Read(data, 0, data.Length);
+
+                return data;
+            }
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="data">二进制数据</param>
+        /// <returns></returns>
+        public static T Deserialize<T>(byte[] data)
+        {
+            using (var ms = new MemoryStream())
+            {
+                // 写入到内存中
+                ms.Write(data, 0, data.Length);
+
+                // 反序列化
+                ms.Position = 0;
+                var value = Serializer.Deserialize<T>(ms);
+                return value;
+            }
+        }
+
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        /// <param name="value">值</param>
+        /// <param name="order">序号</param>
+        /// <returns></returns>
+        public static byte[] GetData(int value, int order)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var writer = ProtoWriter.Create(ms, _typeModel);
+
+                // 写入数据
+                ProtoWriter.WriteFieldHeader(order, WireType.Variant, writer);
+                ProtoWriter.WriteInt32(value, writer);
+
+                // 释放缓冲区
+                writer.Close();
+
+                ms.Position = 0;
+                var data = ms.ToArray();
+
+                return data;
+            }
+        }
+
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        /// <param name="value">值</param>
+        /// <param name="order">序号</param>
+        /// <returns></returns>
+        public static byte[] GetData(uint value, int order)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var writer = ProtoWriter.Create(ms, _typeModel);
+
+                // 写入数据
+                ProtoWriter.WriteFieldHeader(order, WireType.Variant, writer);
+                ProtoWriter.WriteUInt32(value, writer);
+
+                // 释放缓冲区
+                writer.Close();
+
+                ms.Position = 0;
+                var data = ms.ToArray();
+
+                return data;
+            }
+        }
+
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        /// <param name="value">值</param>
+        /// <param name="order">序号</param>
+        /// <returns></returns>
+        public static byte[] GetData(bool value, int order)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var writer = ProtoWriter.Create(ms, _typeModel);
+
+                // 写入数据
+                ProtoWriter.WriteFieldHeader(order, WireType.Variant, writer);
+                ProtoWriter.WriteBoolean(value, writer);
+
+                // 释放缓冲区
+                writer.Close();
+
+                ms.Position = 0;
+                var data = ms.ToArray();
+
+                return data;
+            }
+        }
+
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        /// <param name="value">值</param>
+        /// <param name="order">序号</param>
+        /// <returns></returns>
+        public static byte[] GetData(string value, int order)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var writer = ProtoWriter.Create(ms, _typeModel);
+
+                // 写入数据
+                ProtoWriter.WriteFieldHeader(order, WireType.String, writer);
+                ProtoWriter.WriteString(value, writer);
+
+                // 释放缓冲区
+                writer.Close();
+
+                ms.Position = 0;
+                var data = ms.ToArray();
+
+                return data;
+            }
+        }
+    }
+}
