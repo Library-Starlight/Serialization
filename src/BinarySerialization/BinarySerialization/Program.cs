362916fa8f23449459970e06d8d08e29197c52bd
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-09-03 22:05:56 +0800

    feature
    二进制序列化

---
+++ src/BinarySerialization/BinarySerialization/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BinarySerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            var data = Serialize(list);
+
+            Console.WriteLine(BitConverter.ToString(data));
+            Console.WriteLine();
+
+            var clone = Deserialize<List<int>>(data);
+            Console.WriteLine(JsonConvert.SerializeObject(clone));
+        }
+
+        private static byte[] Serialize(object obj)
+        {
+            var formatter = new BinaryFormatter();
+            var stream = new MemoryStream();
+            formatter.Serialize(stream, obj);
+
+            stream.Position = 0;
+
+            return stream.ToArray();
+        }
+
+        private static T Deserialize<T>(byte[] data)
+        {
+            var formatter = new BinaryFormatter();
+            var stream = new MemoryStream(data);
+            return (T)formatter.Deserialize(stream);
+        }
+    }
+}
